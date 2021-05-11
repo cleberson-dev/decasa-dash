5e513830dea7f98f366feab28db9b1185ee9d08e
cleberson-dev <cleberson.dev@gmail.com>
2021-05-11 10:30:55 -0300

    feat(fornecedores): summarize some suppliers

--- src/app/pages/fornecedores/fornecedores.component.ts
+++ src/app/pages/fornecedores/fornecedores.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Fornecedor } from '../../types';
+
+const defaultData = {
+  bairro: 'Bairro #1',
+  celular: '(99) 99999-9999',
+  cep: '65284-000',
+  cnpj: '123.123.123.123',
+  email: 'email@decasa.com',
+  logradouro: 'Rua 1º de Abril',
+  numero: 123,
+  pontoReferencia: 'Próximo a casa do lado',
+  telefone: '(99) 99999-9999',
+  cpfRepresentante: '123.123.123-12'
+}
 
 @Component({
   selector: 'ngx-fornecedores',
@@ -6,6 +20,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fornecedores.component.scss']
 })
 export class FornecedoresComponent implements OnInit {
+
+  fornecedores: Fornecedor[] = [
+    { 
+      id: 1,
+      nome: 'Fornecedor #1',
+      ...defaultData
+    },
+    { 
+      id: 2,
+      nome: 'Fornecedor #2',
+      ...defaultData
+    },
+    { 
+      id: 3,
+      nome: 'Fornecedor #3',
+      ...defaultData
+    },
+    { 
+      id: 4,
+      nome: 'Fornecedor #4',
+      ...defaultData
+    },
+  ]
 
   constructor() { }
 
