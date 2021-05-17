a7bb9f964cc723adc250a54d83d0a4b405c0aafe
cleberson-dev <cleberson.dev@gmail.com>
2021-05-17 15:55:25 -0300

    feat(pedidos/mapa): Add basic functioning table

--- src/app/pages/pedidos/mapa/mapa.component.ts
+++ src/app/pages/pedidos/mapa/mapa.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Tab } from '../../../components/tabber/tabber.component';
+
+
+type MapRow = {
+  produto: {
+    codigo: string;
+    nome: string;
+    unidade: string;
+    quantidade: number;
+  };
+  precos: (number | undefined)[];
+  menorPrecoIdx?: number;
+}
 
 @Component({
   selector: 'ngx-mapa',
@@ -7,9 +20,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
+  data: MapRow[] = [
+    { 
+      produto: {
+        codigo: "1234",
+        nome: "Produto #1",
+        quantidade: 2,
+        unidade: 'cm'
+      },
+      precos: [
+        1.49,
+        undefined,
+        1.99,
+        0.99
+      ]
+    },
+    { 
+      produto: {
+        codigo: "3333",
+        nome: "Produto #2",
+        quantidade: 1,
+        unidade: 'cm'
+      },
+      precos: [
+        2.98,
+        undefined,
+        1.12,
+        3.45
+      ]
+    }
+  ];
+
+  somaFornecedores: (number | undefined)[] = new Array(this.data[0].precos.length).fill(0);
+  menorSomaIdx: number;
+
+  tabs: Tab[] = [
+    { title: 'Cotação', link: 'cotacao' },
+    { title: 'Mapa', link: 'mapa', active: true  },
+    { title: 'Ordem de compra', link: '' },
+    { title: 'Acompanhamento', link: '' },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
-  }
+    for (let i = 0; i < this.data.length; i += 1) {
+      const { precos } = this.data[i];
 
+      for (let j = 0; j < precos.length; j += 1) {
+        this.somaFornecedores[j] += this.data[i].precos[j] || 0;
+
+        if (precos[j] === undefined) continue;
+        if (this.data[i].menorPrecoIdx === undefined) {
+          this.data[i].menorPrecoIdx = j;
+          continue;
+        }
+
+        if (precos[j] < precos[this.data[i].menorPrecoIdx]) {
+          this.data[i].menorPrecoIdx = j;
+        }
+      }
+    }
+
+    this.menorSomaIdx = this.somaFornecedores.reduce((prevIdx, curVal, curIdx) => {
+      if (
+        (prevIdx === undefined && !Number.isNaN(curVal) && curVal !== 0)
+        || 
+        (prevIdx !== undefined && curVal < this.somaFornecedores[prevIdx]  && curVal !== 0)
+      ) return curIdx;
+      if (curVal === undefined) return prevIdx;
+      return prevIdx;
+    }, 
+    undefined);
+    console.log(this.somaFornecedores, this.menorSomaIdx);
+  }
 }
