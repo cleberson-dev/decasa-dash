d50a565edcc3f40c460e3cdc860654a10eaf35a6
cleberson-dev <cleberson.dev@gmail.com>
2021-05-23 00:42:22 -0300

    fix(estoque/saida): implement basic funcionality

--- src/app/pages/estoque/saida/saida.component.ts
+++ src/app/pages/estoque/saida/saida.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Tab } from '../../../components/tabber/tabber.component';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'ngx-saida',
@@ -6,10 +9,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./saida.component.scss']
 })
 export class SaidaComponent implements OnInit {
+  @ViewChild('autoInput') input;
+  autoOptions: string[];
+  suggestedOptions$: Observable<string[]>;
 
-  constructor() { }
+  tabs: Tab[] = [
+    { title: 'Entrada', link: '/estoque', active: false },
+    { title: 'Saída/Baixa', link: '/estoque/saida', active: true },
+    { title: 'Alerta de Compra', link: '/estoque/alerta-compra', active: false },
+    { title: 'Estoque mínimo', link: '/estoque/minimo', active: false },
+  ];
+
+  data = [];
+
+  form = this.fb.group({
+    codigo: ['', [Validators.required]],
+    nome: ['', [Validators.required]],
+    unidade: ['', [Validators.required]],
+    quantidade: ['', [Validators.required]]
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.autoOptions = ['Produto #1', 'Produto #2', 'Produto #3'];
+    this.suggestedOptions$ = of(this.autoOptions);
   }
 
+  onCodigoBlur() {
+
+  }
+
+  onNomeInputChange() {
+
+  }
+
+  onNomeSelectionChange(event: string) {
+
+  }
+
+  onNomeInputBlur() {
+
+  }
+
+  onProdutoAdd() {
+
+  }
+
+  isInvalidControl(controlName: string) {
+    const control = this.form.controls[controlName];
+    return control.invalid && (control.touched || control.dirty);
+  }
 }
