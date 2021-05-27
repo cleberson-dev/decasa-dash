ac0ff589b52dfb68a0029d8b82bd13a32f96ff3a
cleberson-dev <cleberson.dev@gmail.com>
2021-05-27 12:18:20 -0300

    feat(pedidos/ordem-compra): create table showing products

--- src/app/pages/pedidos/ordem-compra/ordem-compra.component.ts
+++ src/app/pages/pedidos/ordem-compra/ordem-compra.component.ts
@@ -1,5 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Tab } from '../../../components/tabber/tabber.component';
+type RowProps = {
+  codigo: string;
+  produto: string;
+  quantidade: number;
+  unidade: string;
+  precoUnitario: number;
+}
+
+class Row {
+  props: RowProps;
+
+  constructor(props: RowProps) {
+    this.props = props;
+  }
+
+  get subTotal(): number {
+    return this.props.precoUnitario * this.props.quantidade;
+  }
+}
 
 @Component({
   selector: 'ngx-ordem-compra',
@@ -14,9 +33,17 @@ export class OrdemCompraComponent implements OnInit {
     { title: 'Acompanhamento', link: '' },
   ];
 
+  data: Row[] = [
+    new Row({ codigo: '00001', produto: 'Produto #1', quantidade: 4, unidade: 'cm', precoUnitario: 1.99 }),
+    new Row({ codigo: '00002', produto: 'Produto #2', quantidade: 2, unidade: 'cm', precoUnitario: 1.99 })
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  get precoTotal(): number {
+    return this.data.reduce((prev, cur) => prev + cur.subTotal, 0);
+  }
 }
