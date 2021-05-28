b1d9e1f89e2091d7959d69cdcf3165314f79457e
cleberson-dev <cleberson.dev@gmail.com>
2021-05-28 14:54:51 -0300

    fix(vendas): remove unnecessary tabs

--- src/app/pages/vendas/vendas.component.ts
+++ src/app/pages/vendas/vendas.component.ts
@@ -10,10 +10,10 @@ export class VendasComponent implements OnInit {
 
   tabs: Tab[] = [
     { title: 'Venda de Balcão', link: '/vendas', active: true },
-    { title: 'eCommerce', link: '/vendas/eCommerce', active: false },
-    { title: 'Separando', link: '/vendas/separando', active: false },
-    { title: 'Entregando', link: '/vendas/entregando', active: false },
-    { title: 'Entregue', link: '/vendas/entregue', active: false }
+    { title: 'eCommerce', link: '/vendas/ecommerce', active: false },
+    // { title: 'Separando', link: '/vendas/separando', active: false },
+    // { title: 'Entregando', link: '/vendas/entregando', active: false },
+    // { title: 'Entregue', link: '/vendas/entregue', active: false }
   ];
   
   constructor() { }
