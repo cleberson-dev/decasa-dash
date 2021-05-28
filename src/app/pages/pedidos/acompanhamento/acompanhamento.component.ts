ee5a915bc607aa357f6d7ddcfab505103a43d254
cleberson-dev <cleberson.dev@gmail.com>
2021-05-28 01:32:59 -0300

    feat(pedidos/acompanhamento): define basic functionality

--- src/app/pages/pedidos/acompanhamento/acompanhamento.component.ts
+++ src/app/pages/pedidos/acompanhamento/acompanhamento.component.ts
@@ -15,6 +15,11 @@ export class AcompanhamentoComponent implements OnInit {
     { title: 'Acompanhamento', link: '/pedidos/acompanhamento', active: true },
   ];
 
+  mapas = [
+    { codigo: '123456/2021', data: '01/01/1999', solicitante: 'Lorem Ipsum', loja: 'Loja #1' },
+    { codigo: '333333/2021', data: '01/01/1999', solicitante: 'Lorem Ipsum', loja: 'Loja #2' },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
