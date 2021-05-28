3471a786ecee833543b64ab8221db266b653c1b5
cleberson-dev <cleberson.dev@gmail.com>
2021-05-28 16:44:18 -0300

    chore(vendas): uncomment tabs

--- src/app/pages/vendas/vendas.component.ts
+++ src/app/pages/vendas/vendas.component.ts
@@ -11,9 +11,9 @@ export class VendasComponent implements OnInit {
   tabs: Tab[] = [
     { title: 'Venda de Balcão', link: '/vendas', active: true },
     { title: 'eCommerce', link: '/vendas/ecommerce', active: false },
-    // { title: 'Separando', link: '/vendas/separando', active: false },
-    // { title: 'Entregando', link: '/vendas/entregando', active: false },
-    // { title: 'Entregue', link: '/vendas/entregue', active: false }
+    { title: 'Separando', link: '/vendas/separando', active: false },
+    { title: 'Entregando', link: '/vendas/entregando', active: false },
+    { title: 'Entregue', link: '/vendas/entregue', active: false }
   ];
   
   constructor() { }
