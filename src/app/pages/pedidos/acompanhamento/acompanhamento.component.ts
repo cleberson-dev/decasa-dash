0ba347da3f684e518198376e6b363d70f73141a0
cleberson-dev <cleberson.dev@gmail.com>
2021-06-04 16:49:20 -0300

    chore(pedidos/acompanhamentos): show price

--- src/app/pages/pedidos/acompanhamento/acompanhamento.component.ts
+++ src/app/pages/pedidos/acompanhamento/acompanhamento.component.ts
@@ -20,6 +20,23 @@ export class AcompanhamentoComponent implements OnInit {
     { codigo: '333333/2021', data: '01/01/1999', solicitante: 'Lorem Ipsum', loja: 'Loja #2' },
   ];
 
+  ordensDeCompra = [
+    { 
+      codigo: '123456/2021',
+      data: '01/01/1999', 
+      solicitante: 'Lorem Ipsum', 
+      loja: 'Loja #1',
+      preco: 99.99
+    },
+    { 
+      codigo: '333123/2021',
+      data: '02/01/1999', 
+      solicitante: 'Lorem Ipsum', 
+      loja: 'Loja #1',
+      preco: 999.99
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
