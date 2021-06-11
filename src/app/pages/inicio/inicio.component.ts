73916f834be99f5f7209f7146b0849f1faa51516
cleberson-dev <cleberson.dev@gmail.com>
2021-06-11 17:07:36 -0300

    feat(inicio): add lojas section

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
+  lojas = [
+    { nome: 'Loja 01', cnpj: '99.999.999/9999-99', collapsed: false },
+    { nome: 'Loja 02', cnpj: '88.888.888/8888-88', collapsed: false },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
