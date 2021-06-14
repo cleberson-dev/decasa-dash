8d89647dcc592e242d97c89a3ecc9b1a88c6c515
cleberson-dev <cleberson.dev@gmail.com>
2021-06-14 10:06:15 -0300

    feat(clientes): add tab menu

--- src/app/pages/clientes/clientes.component.ts
+++ src/app/pages/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Tab } from '../../components/tabber/tabber.component';
 
 @Component({
   selector: 'ngx-clientes',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clientes.component.scss']
 })
 export class ClientesComponent implements OnInit {
+  tabs: Tab[] = [
+    { title: 'Dados Cadastrais', link: '/clientes', active: true },
+    { title: 'Cupons', link: '/clientes/cupons' },
+    { title: 'Compras', link: '/clientes/compras' },
+    { title: 'Devoluções', link: '/clientes/devolucoes' },
+    { title: 'Endereços', link: '/clientes/enderecos' },
+  ];
 
   constructor() { }
 
