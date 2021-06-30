e29c9c272e6518e98194249eb0a6e968e2dd0d9c
cleberson-dev <cleberson.dev@gmail.com>
2021-06-30 13:33:56 -0300

    fix(api-status-indicator): change button appearance when hovered

--- src/app/components/api-status-indicator/api-status-indicator.component.ts
+++ src/app/components/api-status-indicator/api-status-indicator.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 
-type APIStatuses = "loading" | "working" | "not-working";
+type APIStatuses = "loading" | "working" | "not-working" | "hovered";
 
 @Component({
   selector: 'api-status-indicator',
@@ -10,6 +10,7 @@ type APIStatuses = "loading" | "working" | "not-working";
 })
 export class ApiStatusIndicatorComponent implements OnInit {
   apiStatus: APIStatuses = "loading";
+  previousStatus: APIStatuses = "loading";
 
   constructor(
     private apiService: ApiService,
@@ -36,7 +37,8 @@ export class ApiStatusIndicatorComponent implements OnInit {
     const colors: Record<APIStatuses, string> = {
       "loading": "warning",
       "working": "success",
-      "not-working": "danger"
+      "not-working": "danger",
+      "hovered": "info",
     };
 
     return colors[this.apiStatus];
@@ -46,7 +48,8 @@ export class ApiStatusIndicatorComponent implements OnInit {
     const options: Record<APIStatuses, string> = {
       "loading": "Carregando API",
       "working": "API funcionando",
-      "not-working": "API não está funcionando"
+      "not-working": "API não está funcionando",
+      "hovered": "Atualizar API"
     };
 
     return options[this.apiStatus];
@@ -56,9 +59,19 @@ export class ApiStatusIndicatorComponent implements OnInit {
     const icons: Record<APIStatuses, string> = {
       "loading": "loader-outline",
       "working": "checkmark-outline",
-      "not-working": "close-outline"
+      "not-working": "close-outline",
+      "hovered": "refresh-outline",
     };
 
     return icons[this.apiStatus];
+  }
+
+  onHover() {
+    this.previousStatus = this.apiStatus;
+    this.apiStatus = "hovered";
+  }
+
+  onLeave() {
+    this.apiStatus = this.previousStatus;
   }
 }
