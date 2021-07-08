86d15b6f713ae5f1b023d777d7d1144ee37f3802
cleberson-dev <cleberson.dev@gmail.com>
2021-07-08 14:50:02 -0300

    test(e2e): write 3 test cases for login page

---
+++ cypress/integration/LoginPage.spec.ts
@@ -0,0 +1,27 @@
+describe('Página de Login do Lojista', () => {
+  it('Deve somente carregar de forma sucedida', () => {
+    cy.visit('/login');
+    cy.contains('Entre no Marketplace');
+  });
+
+  it('Deve habilitar o botão de confirmação para dados válidos', () => {
+    cy.get('input[type="email"]').first()
+      .type('meuemail@meuemail.com');
+
+    cy.get('input[type="password"]').first()
+      .type('minhasenha123');
+  });
+
+  it('Deve desabilitar o botão de confirmação para dados inválidos', () => {
+    cy.get('input[type="email"]').first()
+      .clear()
+      .type('emailinvalido');
+
+    cy.get('input[type="password"]').first()
+      .clear();
+
+    cy.get('button[type="submit"]').first()
+      .should('be.disabled');
+  });
+});
+
