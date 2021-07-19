4b18b0b333a727c36d411c4fa3f4055cb1ea6406
cleberson-dev <cleberson.dev@gmail.com>
2021-07-18 21:46:01 -0300

    feat(produto): create menu

--- src/app/components/produto/produto.component.ts
+++ src/app/components/produto/produto.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
-
-type Option = {
-  title: string;
-};
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { NbMenuItem, NbMenuService } from '@nebular/theme';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'ngx-produto',
@@ -11,14 +9,26 @@ type Option = {
 })
 export class ProdutoComponent implements OnInit {
   @Input() produto: Produto;
-  @Input() options: Option[] = [];
+  @Input() options: NbMenuItem[] = [
+    { title: 'Bloquear', icon: 'close-outline' }
+  ];
   @Input() valor?: number;
   @Input() estoque?: number;
 
+  @Output() block = new EventEmitter<Produto>();
+
   defaultImage = "https://www.eppendorf.com/fileadmin/General/MyEppendorf/Productregistration/Update_January_2021/Shop_ICON_Final.jpg";
   
-  constructor() { }
+  constructor(
+    private menuService: NbMenuService,
+  ) { }
 
   ngOnInit(): void {
+    this.menuService.onItemClick()
+        .pipe(filter(({ tag }) => tag === 'product-menu'))
+        .subscribe(({ item }) => {
+          if (item.title !== 'Bloquear') return;
+          this.block.emit(this.produto);
+        });
   }
 }
